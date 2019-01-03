d74a4dc6a2a4e77e4d2f484f545c92dc98413a16
Dan Hutchinson <danhutch5280@gmail.com>
2019-01-02 20:13:29 -0700

    refactor location service

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,21 +1,20 @@
 class LocationService
 
 	def get_coords(location)
-		response = get_coords_response(location)
+		response = get_json("/maps/api/geocode/json?key=#{ENV["GOOGLE_API_KEY"]}&address=#{location}")
 		coords = response[:results].first[:geometry][:location]
 	end
 
 	private
 
-	def get_coords_response(location)
-		response = conn.get("/maps/api/geocode/json?key=#{ENV["GOOGLE_API_KEY"]}&address=#{location}")
+	def get_json(url)
+		response = conn.get(url)
 		parsed = JSON.parse(response.body, symbolize_names: true)
 	end
 
 	def conn
 		Faraday.new(url: "https://maps.googleapis.com") do |faraday|
 			faraday.adapter Faraday.default_adapter
-
 		end
 	end
 
