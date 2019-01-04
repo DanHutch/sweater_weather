c3d415f2692db8031953836089a77c04c3eee8dc
Dan Hutchinson <danhutch5280@gmail.com>
2019-01-04 11:18:58 -0700

    add more robust request testing

--- spec/requests/API/v1/daily_gif_request_spec.rb
+++ spec/requests/API/v1/daily_gif_request_spec.rb
@@ -13,9 +13,18 @@ RSpec.describe 'GET /api/v1/gifs' do
     expect(response).to be_successful
 
     results = JSON.parse(response.body, symbolize_names: true)
-binding.pry
 
+    expect(results).to have_key(:data)
+    expect(results[:data][:attributes]).to have_key(:daily_forecasts)
+    expect(results[:data][:attributes][:daily_forecasts]).to be_a Array
+    expect(results[:data][:attributes]).to have_key(:copyright)
 
+    results[:data][:attributes][:daily_forecasts].each do |daily|
+      expect(daily).to be_a Hash
+      expect(daily).to have_key(:time)
+      expect(daily).to have_key(:summary)
+      expect(daily).to have_key(:url)
+    end
 
   end
 
