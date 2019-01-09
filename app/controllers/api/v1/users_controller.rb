cdb0e1a099bb43cf41ab60c944ff5cb944bf56c4
Dan Hutchinson <danhutch5280@gmail.com>
2019-01-08 18:43:24 -0700

    refactor favorites controller authentication

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,17 +3,21 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      render json: UserKeySerializer.new(@user), status: 201
-    else
-      render json: "Something went wrong!", status: 422
-    end
+    attempt_save_user(@user)
   end
 
   private
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def attempt_save_user(user)
+    if user.save
+      render json: UserKeySerializer.new(@user), status: 201
+    else
+      render json: "Something went wrong!", status: 422
+    end
   end
 
 end
